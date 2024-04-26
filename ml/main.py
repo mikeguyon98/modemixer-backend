909f68a0f1f1d718f602c79814bf43505b6102a4
Michael Guyon <mikeguyon98@gmail.com>
2024-04-25 21:41:52 -0400

    creates mongo_vectorstore class

--- ml/main.py
+++ ml/main.py
@@ -7,6 +7,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pymongo import MongoClient
+from mongo_vectorstore import MongoVectorStore
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="../.env")
@@ -38,12 +39,14 @@ print(docs[0])
 #     index_name = vector_search_index
 # )
 
-vector_search = MongoDBAtlasVectorSearch.from_connection_string(
-    connection_string=ATLAS_CONNECTION_STRING,
-    namespace="test.vector",
-    embedding=OpenAIEmbeddings(disallowed_special=()),
-    index_name=vector_search_index
-)  
+# vector_search = MongoDBAtlasVectorSearch.from_connection_string(
+#     connection_string=ATLAS_CONNECTION_STRING,
+#     namespace="test.vector",
+#     embedding=OpenAIEmbeddings(disallowed_special=()),
+#     index_name=vector_search_index
+# )  
+
+vector_search = MongoVectorStore(ATLAS_CONNECTION_STRING, db_name, collection_name, vector_search_index).initialize()
 
 query = "MongoDB Atlas security"
 results = vector_search.similarity_search(query)
